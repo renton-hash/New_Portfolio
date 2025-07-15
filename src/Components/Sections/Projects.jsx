026fef46dabca2c662b5e40f3c0cfe24442961a9
renton-hash <163464787+renton-hash@users.noreply.github.com>
2025-07-15 23:49:38 +0100

    Update Projects.jsx

--- src/Components/Sections/Projects.jsx
+++ src/Components/Sections/Projects.jsx
@@ -43,34 +43,6 @@ return(
 </div>
 
 
-<div className="p-6 rounded-xl border-white/10 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-<h3 className="text-xl font-bold mb-2">Secure-Trade</h3>
- <p className="text-gray-400 mb-4">
-
-    An ongoing project which will function as an intermediary between buyers and sellers
- </p>
-    <div>
-        {["Nextjs", "Typescript", "TailwindCSS"].map((tech, key) => {
-
-return(
-    <span 
-    key={key} 
-    className="bg-blue-500/10 text-blue-500 py-1 px-3  rounded-full text-sm 
-    hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-            {tech}
-    </span>
-    );
-        } )}
-    </div>
-
-    <div className="flex justify-between items-center">
-        <a href="https://securetrade.vercel.app/" 
-        className="text-blue-400 hover:text-blue-300 transition-colors my-4
-        ">
-            {" "}
-            View Project {" "} </a>
-            </div>
-
             <div className="p-6 rounded-xl border-white/10 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
 <h3 className="text-xl font-bold mb-2">holyfamilycharities</h3>
  <p className="text-gray-400 mb-4">
