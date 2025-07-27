b381e724288c909c34b1f6705f1516924444b353
renton <adegbileanthony@gmail.com>
2025-07-27 13:40:13 +0100

    I keep going

--- src/Components/Sections/Contact.jsx
+++ src/Components/Sections/Contact.jsx
@@ -54,7 +54,7 @@ export const Contact = () => {
             </button>
           </form>
                   <div>
-          <h2 className="text-3xl sm:text-4xl font-bold mb-6 text-center bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">
+          <h2 className="text-3xl mt-12 sm:text-4xl font-bold mb-6 text-center bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">
             Phone Contact
           </h2>
             <a href="https://wa.me/+2348034605554
--- src/Components/Sections/Projects.jsx
+++ src/Components/Sections/Projects.jsx
@@ -61,6 +61,32 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+                
+                {/* Project 3 */}
+                {/*
+            <div className="p-6 rounded-xl border-white/10 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Secure_trade</h3>
+              <p className="text-gray-400 mb-4">This is a platform to secure exchange between Sellers and Buyers</p>
+              <div className="flex flex-wrap gap-2">
+                {["NextJS", "Typescript", "TailwindCSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))} 
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://giantstepstrasjoschool.com"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project
+                </a>
+              </div>
+            </div>
+          */}
           </div>
         </div>
       </OnScroll>
